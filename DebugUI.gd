01c84857a596fe84030573ba329bab17a9305356
Rowan-Nag <rowan_nag@icloud.com>
2024-02-04 22:23:26 -0500

    added debug UI

--- DebugUI.gd
+++ DebugUI.gd
@@ -10,7 +10,7 @@ func _ready():
 	label.text = "Player Velocity:"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if(player):
 		label.text = "Player Velocity: " + str(player.velocity) + "\n"
 		label.text += "Player State: " + str(player.state_machine.current_state.name)
