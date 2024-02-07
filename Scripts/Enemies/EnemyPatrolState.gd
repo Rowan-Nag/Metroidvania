6790678d37b89fb1c3127bdfae75cbe7bddd4105
Rowan-Nag <rowan_nag@icloud.com>
2024-02-07 00:13:08 -0500

    Added patrol states.

---
+++ Scripts/Enemies/EnemyPatrolState.gd
@@ -0,0 +1,68 @@
+extends State
+
+@export var alerted_state : State 
+
+@export var start_facing_right: bool = true
+@export var alert_distance : int = 400
+@export var walkSpeed = 200
+@export var waitTime : float = 2
+
+@onready var wallDetector : RayCast2D 
+@onready var floorDetector : RayCast2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var waiting = false
+
+func enter() -> void:
+	parent.play_animation("walk")
+	wallDetector = parent.find_child("wallDetector")
+	floorDetector = parent.find_child("floorDetector")
+
+func exit() -> void:
+	pass
+
+#func process_input(event: InputEvent) -> State:
+	#return null
+#
+#func process_frame(delta: float) -> State:
+	#return null
+
+func process_physics(delta: float) -> State:
+	parent.velocity.y += gravity*delta
+	if(not waiting):
+		parent.velocity.x = walkSpeed
+		
+		if(parent.getPlayerDistance() < alert_distance):
+			if(alerted_state):
+				
+				return alerted_state
+		if(not floorDetector.is_colliding()):
+			print("Floor gone.")
+		if(not floorDetector.is_colliding()):
+			#TODO: Find out why it's not detector the floor???
+			#print(wallDetector.is_colliding(), not floorDetector.is_colliding(), wallDetector.target_position.x)
+			wait_and_turn()
+		
+		
+		
+	else:
+		parent.velocity.x = 0
+		parent.play_animation("idle")
+		
+	parent.move_and_slide()
+	return null
+
+func play_animation(animation) -> void:
+	if parent.animations:
+		parent.animations.play(animation)
+		
+
+func wait_and_turn():
+	waiting = true
+	await get_tree().create_timer(waitTime).timeout
+	wallDetector.target_position.x = -wallDetector.target_position.x
+	floorDetector.position.x = - floorDetector.position.x
+	walkSpeed = -walkSpeed
+	parent.animated_sprite.scale.x = -parent.animated_sprite.scale.x
+	waiting = false
