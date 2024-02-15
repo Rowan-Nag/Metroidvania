72393e1d4d464c0d73ea0437f6df365585c86464
Rowan-Nag <rowan_nag@icloud.com>
2024-02-15 11:57:52 -0500

    ADded wallcling & wallcling animation

--- Scripts/Player/PlayerWallcling.gd
+++ Scripts/Player/PlayerWallcling.gd
@@ -3,8 +3,7 @@ extends State
 @export var ground_state: State
 @export var jump_state: State
 @export var dash_state: State
-@export var attack_state : State
-@export var wallcling_state : State
+@export var fall_state : State
 
 @export var gravityMultiplier: float = 1
 @export var dragMultiplier: float = 1
@@ -13,18 +12,29 @@ extends State
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity") * gravityMultiplier
 
+var adjacent_wall : int # negative: left wall, positive: right wall
 
 var inputDir: float
 
 func enter() -> void:
+	print("Entered!")
+	parent.velocity.y = 0
+
+	play_animation("wallcling")
+	inputDir = Input.get_axis("Left", "Right")
+	adjacent_wall = inputDir
+	parent.animations.scale.x = -sign(adjacent_wall)*abs(parent.animations.scale.x)
 	super()
 
 func process_input(event: InputEvent) -> State:
 	if Input.is_action_just_pressed('Dash'):
 		return dash_state
-	if Input.is_action_just_pressed("Attack"):
-		return attack_state
-		
+	#if Input.is_action_just_pressed("Attack"): # Can't attack while wall clinging
+		#return attack_state 
+	if Input.is_action_just_pressed("Jump"):
+		parent.velocity.x = -adjacent_wall*200
+		return jump_state
+		#Walljump
 	
 	
 	return null
@@ -37,34 +47,17 @@ func process_physics(delta: float) -> State:
 	
 	inputDir = Input.get_axis("Left", "Right")
 	
-	
-	
-	if(inputDir):
-		
-		parent.velocity.x = move_toward(parent.velocity.x, inputDir*maxSpeed, acceleration*delta)
-	else:
-		parent.velocity.x = move_toward(parent.velocity.x, 0, drag*delta)
-	
 	#Gravity (vertical movement)
 	parent.velocity.y += gravity*gravityMultiplier*delta
 	parent.velocity.y = clamp(parent.velocity.y, -10000, parent.terminal_velocity)
 	
 	parent.move_and_slide()
 	
-	# Animations
-	if(inputDir == 0):
-		play_animation("idle")
-	else:
-		play_animation("walk")
-		parent.animations.scale.x = sign(inputDir)*abs(parent.animations.scale.x)
-	
 	# State switches
-	if(inputDir):
-		var canWalljump = parent.getAdjacentWalls()
-		print(canWalljump.left)
-		if(canWalljump.left or canWalljump.right):
-			return wallcling_state
 	
+	if(sign(inputDir) != sign(adjacent_wall)):
+		return fall_state
 	if parent.is_on_floor():
 		return ground_state
+	
 	return null
