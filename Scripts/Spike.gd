b3eafb2e49df840d7a5b61797a0d9a59cb1bcc2f
Rowan-Nag <rowan_nag@icloud.com>
2024-06-17 16:42:15 -0700

    fadein/out on player fall

--- Scripts/Spike.gd
+++ Scripts/Spike.gd
@@ -2,4 +2,8 @@ extends Sprite2D
 
 func _on_area_2d_body_entered(body):
 	if (body is Player): 
+		body.take_damage(1, Vector2.ZERO)
+		Global.Game.fade_animation_player.play("fade_out")
+		await get_tree().create_timer(0.1).timeout
+		Global.Game.fade_animation_player.play("fade_in")
 		Global.Game.reset_player_to_checkpoint()
