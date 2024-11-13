f636accffcb33dd28ebafd0a8a4065deb37f902a
Rowan-Nag <rowan_nag@icloud.com>
2024-11-13 16:44:28 -0500

    audio fade in/out, audio menu

--- GameManager.gd
+++ GameManager.gd
@@ -35,6 +35,7 @@ func switch_to_scene(new_scene_path : String, door_name : String):
 			load_new_scene(new_scene_path)
 			current_scene = new_scene.instantiate()
 		add_child(current_scene)
+		fade_all_audio_in()
 		#print(" to " + current_scene.name)
 		#Fade back in
 		var door = current_scene.find_child(door_name)
@@ -54,7 +55,7 @@ func switch_to_scene(new_scene_path : String, door_name : String):
 		print(Global.activeCamera.limit_left)
 		Global.activeCamera.reset_smoothing()
 		Global.player.snap_to_ground(30)
-
+		
 		fade_in()
 
 
@@ -80,9 +81,35 @@ func modulate_background(modulation : Color):
 		background.modulate = modulation
 
 func fade_out():
+	fade_all_audio_out()
 	Global.fade_out.emit()
 	await get_tree().create_timer(0.2).timeout
 	
 func fade_in():
 	Global.fade_in.emit()
 	await get_tree().create_timer(0.1).timeout
+
+func fade_all_audio_out(scene_root : Node = current_scene):
+	var audioPlayers : Array[AudioStreamPlayer]
+	audioPlayers.assign(scene_root.find_children('*','AudioStreamPlayer',true))
+	# get all audioplayers in the new scene
+	var audio_fade_tween : Tween = get_tree().create_tween()
+	for asp : AudioStreamPlayer in audioPlayers:
+		audio_fade_tween.tween_property(asp,'volume_db',-60,1)
+		#begin tweening their volumes to 0
+
+func fade_all_audio_in(scene_root : Node = current_scene):
+	var audioPlayers : Array[AudioStreamPlayer]
+	audioPlayers.assign(scene_root.find_children('*','AudioStreamPlayer',true))
+	# get all audioplayers in the new scene
+	var audio_fade_tween : Tween = get_tree().create_tween()
+	
+	for asp : AudioStreamPlayer in audioPlayers:
+		var default_vol = asp.volume_db 
+		# store the final (default) volume of player
+		asp.volume_db = -60 # begin from -60db (inaudible)
+		audio_fade_tween.tween_property(asp,'volume_db',default_vol,0.5)
+		audio_fade_tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+		#begin tweening their volumes to the default
+		
+	
--- SettingsMenu.gd
+++ SettingsMenu.gd
@@ -9,15 +9,15 @@ func _ready():
 	master_volume.ratio = 0.5
 	music_volume.ratio = 1.0
 	effects_volume.ratio = 1.0
+	_update_audio()
 	
-
-func _set_master_audio(_val):
+func _update_audio():
 	AudioServer.set_bus_volume_db(0, linear_to_db(master_volume.ratio))
-	#print("audio: " + str(AudioServer.get_bus_volume_db(0)))
-
-func _set_music_audio(_val):
 	AudioServer.set_bus_volume_db(1, linear_to_db(music_volume.ratio))
-
-func _set_effects_audio(_val):
 	AudioServer.set_bus_volume_db(2, linear_to_db(effects_volume.ratio))
+
+
+func _on_volume_slider_changed(_val):
+	_update_audio()
+
 	
